b63660e261cd306f99246a7df9a00826fa987215
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-03 16:53:00 -0700

    iOS

---
+++ PictureMeApp/Controllers/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  PictureMeApp
+//
+//  Created by Anton Veldanov on 10/3/21.
+//
+
+import UIKit
+
+
+class MainTabController: UITabBarController{
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
