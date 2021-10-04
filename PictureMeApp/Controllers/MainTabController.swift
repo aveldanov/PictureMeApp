3d93672ab1c5ae352aa78b879e6c9ced9730af26
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-03 17:39:41 -0700

    iOS

--- PictureMeApp/Controllers/MainTabController.swift
+++ PictureMeApp/Controllers/MainTabController.swift
@@ -25,7 +25,6 @@ class MainTabController: UITabBarController{
 //        view.backgroundColor = .white
 
         let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
-        
         let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
         let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
         let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationsController())
