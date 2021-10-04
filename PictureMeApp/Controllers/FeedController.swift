012ba5a863ef7d0192bcdbf8735f817d41363e9c
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-03 18:26:02 -0700

    iOS

--- PictureMeApp/Controllers/FeedController.swift
+++ PictureMeApp/Controllers/FeedController.swift
@@ -47,9 +47,12 @@ extension FeedController {
 }
 
 
+ //MARK: UICollectionViewDelegateFlowLayout
 
 extension FeedController: UICollectionViewDelegateFlowLayout{
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
     
     
     
