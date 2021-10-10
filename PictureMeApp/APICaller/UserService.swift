4aa158a1f14e417bf8737b28da815d6af3a4985a
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-09 18:10:41 -0700

    iOS

---
+++ PictureMeApp/APICaller/UserService.swift
@@ -0,0 +1,20 @@
+//
+//  UserService.swift
+//  PictureMeApp
+//
+//  Created by Anton Veldanov on 10/9/21.
+//
+
+import Foundation
+
+
+struct UserService{
+    static func fetchUser(){
+        
+        
+        
+        
+    }
+    
+    
+}
---
+++ PictureMeApp/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  PictureMeApp
+//
+//  Created by Anton Veldanov on 10/9/21.
+//
+
+import Foundation
