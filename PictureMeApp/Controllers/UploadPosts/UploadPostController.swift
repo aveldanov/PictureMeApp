4f865930ddc6ac81044c4324bb9cde42c10f99ce
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-15 08:37:22 -0700

    iOS

--- PictureMeApp/Controllers/UploadPosts/UploadPostController.swift
+++ PictureMeApp/Controllers/UploadPosts/UploadPostController.swift
@@ -23,9 +23,17 @@ class UploadPostController: UIViewController{
     
     private let captionTextView: UITextView = {
         let textView = UITextView()
-        
+        textView.backgroundColor = .red
         
         return textView
+    }()
+    
+    private let characterCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "0/100"
+        return label
     }()
     
     
@@ -44,7 +52,7 @@ class UploadPostController: UIViewController{
     
     private func configureUI(){
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
         
         navigationItem.title = "Upload Post"
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
@@ -53,13 +61,16 @@ class UploadPostController: UIViewController{
         
         view.addSubview(photoImageView)
         photoImageView.setDimensions(height: 180, width: 180)
-        photoImageView.anchor(top: view.topAnchor)
+        photoImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 8)
         photoImageView.centerX(inView: view)
         photoImageView.layer.cornerRadius = 10
         
-        view.addSubview(captionTextView)
         
+        view.addSubview(captionTextView)
         captionTextView.anchor(top: photoImageView.bottomAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 12, paddingRight: 12,  height: 64)
+        
+        view.addSubview(characterCountLabel)
+        characterCountLabel.anchor(bottom: captionTextView.bottomAnchor, right: view.rightAnchor, paddingRight: 12)
         
     }
     
