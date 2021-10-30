bd1abe0e37e99eba5bc07959d70f0e08e8cb940d
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-29 19:22:30 -0700

    iOS

--- PictureMeApp/APICaller/PostService.swift
+++ PictureMeApp/APICaller/PostService.swift
@@ -46,5 +46,22 @@ struct PostService{
     }
     
     
+    static func fetchPosts(forUser uid: String,completion: @escaping([Post])->Void ){
+        let query = COLLECTION_POSTS
+            .order(by: "timestamp", descending: true)
+            .whereField(uid, isEqualTo: uid)
+        
+        query.getDocuments{(snapshot, error) in
+            guard let documents = snapshot?.documents else{
+                return
+            }
+            
+            let posts = documents.map{Post(postID: $0.documentID, dict: $0.data())}
+            completion(posts)
+            
+        }
+        
+    }
+    
     
 }
